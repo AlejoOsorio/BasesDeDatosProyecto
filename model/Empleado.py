7b6449775af84f7a1c09440f661d6dd542066809
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2024-04-30 11:43:12 -0500

    Creacion del modelo
    Se inicio la creacion del modelo, solo el Empleado tiene contenido

---
+++ model/Empleado.py
@@ -0,0 +1,9 @@
+class Empleado:
+    def __init__(self, nombre, apellido, cedula, direccion, telefono, cargo, profesion):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cedula = cedula
+        self.direccion = direccion
+        self.telefono = telefono
+        self.cargo = cargo
+        self.profesion = profesion
