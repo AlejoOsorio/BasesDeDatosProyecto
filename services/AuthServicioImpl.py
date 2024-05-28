9c5b6584af96b64b4f4279a930dadc10e26d7a45
Sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2024-05-27 22:28:14 -0500

    Autenticación service

---
+++ services/AuthServicioImpl.py
@@ -0,0 +1,9 @@
+import UsuarioServicioImpl
+
+def login(nombreUsuario: str, claveUsuario: str):
+    usuario = UsuarioServicioImpl.obtenerUsuario(nombreUsuario)
+
+    if usuario.claveUsuario != claveUsuario:
+        raise RuntimeError("Usuario/Clave incorrecta")
+
+    return True
